e156c5481bfb83b04987e753dc0f79ebe5ee13bc
itxNaeem <naeem.akram@ideofuzion.com>
2021-07-27 18:36:27 +0500

    code update

---
+++ build/precache-manifest.76070beb75abdc1c270f9264f10ce015.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: '5bf046d0493f7558bcdc73c2211a03f4',
+    url: '/index.html',
+  },
+  {
+    revision: '00aaed4528d7b8f877b1',
+    url: '/static/css/2.3a0ac4c9.chunk.css',
+  },
+  {
+    revision: '008bb64991881d760de1',
+    url: '/static/css/main.79052ad6.chunk.css',
+  },
+  {
+    revision: '00aaed4528d7b8f877b1',
+    url: '/static/js/2.bacac568.chunk.js',
+  },
+  {
+    revision: '958608aaef3e81c9b20a22457416f406',
+    url: '/static/js/2.bacac568.chunk.js.LICENSE.txt',
+  },
+  {
+    revision: '008bb64991881d760de1',
+    url: '/static/js/main.f0ee932e.chunk.js',
+  },
+  {
+    revision: 'd3ebb56a3098223b5316',
+    url: '/static/js/runtime-main.c7319a82.js',
+  },
+  {
+    revision: '49174c736a9c871baaeb2db6d5372eee',
+    url: '/static/media/logo.49174c73.svg',
+  },
+]);
